5bc64d6387ddd8e65449cfb638baec9c4d4c8b42
Jason Stajich <jasonstajich.phd@gmail.com>
2020-10-21 15:05:16 -0700

    timetree info

---
+++ timetree_draw.R
@@ -0,0 +1,17 @@
+library(ape)
+library(phytools)
+library(tidyverse)
+library(RColorBrewer)
+library(ggtree)
+library(deeptime)
+BYTree <- read.tree("BY_r8s.tre")
+BYTree$root.time <- 485
+p = ggtree(BYTree,branch.length="TREE",
+           position = position_nudge(x =-1 * BYTree$root.time)) +
+  geom_tiplab(align=TRUE, linesize=.5) + 
+  coord_geo(xlim = c(-485,0), ylim = c(.75,Ntip(BYTree)), neg = TRUE) +
+  scale_x_continuous(breaks=seq(-485,200,50), labels=abs(seq(-485,200,50))) +
+	theme_tree2()
+p
+
+ggsave("BY_TimeTree.pdf",p,width=8)
